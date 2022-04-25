6f1bd299701b8d1a8131f0c0a22417567009dd09
Екатерина Яроцкая <Ekaterinavar1998@gmail.com>
2022-04-25 18:19:47 +0300

    Maven do not see tests

---
+++ src/test/java/AllureTest.java
@@ -0,0 +1,27 @@
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+
+import static java.lang.String.format;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class AllureTest {
+    @Step
+    public int sunn(int a, int b) {
+        return a + b;
+    }
+
+    @Test
+    public void additionOfNumbers() {
+
+        assertThat(sunn(1, 3), is(4));
+    }
+    @Test
+    public void additionOfNumbersFail() {
+
+        assertThat(sunn(1, 3), is(14));
+    }
+}
